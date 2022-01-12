efd4716e70dbc4cffa85a5f79aff00682b24e498
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-01-12 12:50:44 +0200

    navigation - start

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
-
 function App() {
     return (
         <div className='App'>
-            <h1>Let's Start, Good Luck</h1>
+            <Navigation />
         </div>
     );
 }
+
+const Navigation = () => {
+    return (
+        <nav className='nav'>
+            <ul>
+                <li>
+                    <a href='#'>My Games</a>
+                </li>
+                <li>
+                    <a href='#'>Publick Games</a>
+                </li>
+                <li>
+                    <a href='#'>Pocket FEED</a>
+                </li>
+                <li>
+                    <a href='#'>Tournaments</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <a href='#'>Welcome Email Adress</a>
+                </li>
+                <li>
+                    <a href='#'>Logout</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
 
 export default App;
