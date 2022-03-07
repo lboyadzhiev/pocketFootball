e2d53102ff0d20e931ea07419d3ad45e1fd60ce6
lboyadzhiev <l.boyadzhievv@abv.bg>
2022-03-07 11:06:16 +0200

    image insert

--- client/src/components/Sections/Games/GameContent.js
+++ client/src/components/Sections/Games/GameContent.js
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import './GameContent.css';
+
 const GameContent = () => {
-    return <div></div>;
+    return (
+        <div>
+            <Img />
+        </div>
+    );
 };
 
 export default GameContent;
 
-const img = () => {};
+const Img = () => {
+    return (
+        <div>
+            <img
+                className='game-image'
+                src='https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
+                alt='profileImage'
+            />
+        </div>
+    );
+};
+
+const GameInfo = () => {
+    return <div></div>;
+};
